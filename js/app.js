7e1c27e721b93e54462538e654ff10cbaa24f7c6
PabloGastonMedina <pablogastonmedina@gmail.com>
2024-04-13 01:02:07 -0300

    Realizando las ultimas funciones y modificando la base de datos

--- js/app.js
+++ js/app.js
@@ -1,23 +1,79 @@
 //VARIABLES
-const resultado = document.querySelector("#resultado")
-const year = document.querySelector(".years_select")
+const marca = document.querySelector("#marca");
+const minimo = document.querySelector("#minimo")
+const maximo = document.querySelector("#maximo")
+const ram = document.querySelector("#ram")
+const pantalla = document.querySelector("#pantalla")
+const color = document.querySelector("#color")
+const year = document.querySelector(".years_select");
+
+//contenedor de resultados
+const resultado = document.querySelector("#resultado");
+
 
 const max = new Date().getFullYear();
 const min = max - 9;
 
+//VAMOS A GENERAR UN OBJETO CON LA BUSQUEDA
+const datosBusqueda = {
+    marca : "",
+    years: "",
+    minimo: "",
+    maximo: "",
+    ram: "",
+    pantalla: "",
+    color: "",
+}
 
 //EVENTOS
 document.addEventListener("DOMContentLoaded", () =>{
-    mostrarCelulares(); //mostramos los celulares al cargar
+    mostrarCelulares(celulares); //mostramos los celulares al cargar
 
     llenarSelect(); //llenamos los años en select
 })
+
+// Event listener para los select de busqueda
+marca.addEventListener("change", (event) =>{
+    datosBusqueda.marca = event.target.value;
+    filtraCelular();
+});
+year.addEventListener("change", (event) =>{
+    datosBusqueda.years = parseInt(event.target.value);
+    filtraCelular();
+});
+minimo.addEventListener("change", (event) =>{
+    datosBusqueda.minimo = event.target.value;
+    filtraCelular();
+});
+maximo.addEventListener("change", (event) =>{
+    datosBusqueda.maximo = event.target.value;
+    filtraCelular();
+});
+ram.addEventListener("change", (event) =>{
+    datosBusqueda.ram = event.target.value;
+    filtraCelular();
+});
+pantalla.addEventListener("change", (event) =>{
+    datosBusqueda.pantalla = event.target.value;
+    filtraCelular();
+});
+color.addEventListener("change", (event) =>{
+    datosBusqueda.color = event.target.value;
+    filtraCelular();
+    console.log(datosBusqueda)
+});
+
+
+
 
 
 
 
 //FUNCIONES
-function mostrarCelulares() {
+function mostrarCelulares(celulares) {
+
+    limpiarHTML(); //Elimina el html previo
+
     celulares.forEach( celular =>{
         const {marca, modelo, years, precio, ram, pantalla, color} = celular;
        const celularHTML = document.createElement("p");
@@ -27,13 +83,19 @@ function mostrarCelulares() {
        `;
 
        resultado.appendChild(celularHTML)
-
-
     })
-
-  
 }
 
+//Limpiamos html
+function limpiarHTML () {
+    while(resultado.firstChild){
+        resultado.removeChild(resultado.firstChild);
+    }
+
+}
+
+
+//Llenamos los select con los años 
 function llenarSelect() {
     
     for( let i = max; i >= min; i--){
@@ -42,4 +104,81 @@ function llenarSelect() {
         opcion.textContent = i;
         year.appendChild(opcion);
     }
+}
+
+//Funcion para filtra en base a la busqueda
+function filtraCelular() {
+    const resultado = celulares.filter(filtrarMarca).filter(filtrarYear).filter(filtrarMinimo).filter(filtrarMaximo).filter(filtrarRam).filter(filtrarPatalla).filter(filtrarColor);
+
+    if (resultado.length) {
+        mostrarCelulares(resultado);
+    } else{
+        noResultado();
+    }
+}
+
+function noResultado (){
+    limpiarHTML();
+
+    const noResultado = document.createElement("div");
+    noResultado.textContent = "NO HAY RESULTADOS";
+    noResultado.classList.add("noHayResultado")
+    resultado.appendChild(noResultado);
+}
+
+function filtrarMarca (celular) {
+    const { marca } = datosBusqueda;
+    if ( marca ) {
+        return celular.marca === marca;
+    }
+    return celular;
+}
+
+function filtrarYear (celular) {
+    const { years } = datosBusqueda;
+    if ( years ) {
+        return celular.years === years;
+    }
+    return celular;
+}
+
+function filtrarMinimo (celular) {
+    const { minimo } = datosBusqueda;
+    if ( minimo ) {
+        return celular.precio >= minimo;
+    }
+    return celular;
+
+}
+
+function filtrarMaximo (celular){
+    const { maximo } = datosBusqueda;
+    if ( maximo ) {
+        return celular.precio <= maximo;
+    }
+    return celular;
+}
+
+function filtrarRam ( celular){
+    const { ram } = datosBusqueda;
+    if ( ram ) {
+        return celular.ram == ram;
+    }
+    return celular;
+}
+
+function filtrarPatalla (celular){
+    const { pantalla } = datosBusqueda;
+    if ( pantalla ) {
+        return celular.pantalla === pantalla;
+    }
+    return celular;
+}
+
+function filtrarColor (celular){
+    const { color } = datosBusqueda;
+    if ( color ) {
+        return celular.color === color;
+    }
+    return celular;
 }
--- js/db.js
+++ js/db.js
@@ -1,182 +1,182 @@
 const celulares = [
 	{
 		marca: 'Samsung',
-		modelo: 'a51',
+		modelo: 'Galaxy A04',
+		years: 2023,
+		precio: 150000,
+		ram: 2,
+		pantalla: 'moled',
+		color: 'Negro'
+	},
+    {
+		marca: 'Samsung',
+		modelo: 'Galaxy A14',
+		years: 2024,
+		precio: 235000,
+		ram: 2,
+		pantalla: 'moled',
+		color: 'Negro'
+	},
+    {
+		marca: 'Samsung',
+		modelo: 'Galaxy S23 FE',
+		years: 2024,
+		precio: 820000,
+		ram: 8,
+		pantalla: ' amoled',
+		color: 'Azul'
+	},
+    {
+		marca: 'Samsung',
+		modelo: 'Galaxy Z Fold5',
+		years: 2023,
+		precio: 100000,
+		ram: 12,
+		pantalla: 'lcd',
+		color: 'Negro'
+	},
+    {
+		marca: 'Apple',
+		modelo: 'Iphone 14',
+		years: 2024,
+		precio: 1220000,
+		ram: 6,
+		pantalla: 'ips',
+		color: 'Negro'
+	},
+    {
+		marca: 'Apple',
+		modelo: 'Iphone 13',
 		years: 2019,
+		precio: 20000,
+		ram: 4,
+		pantalla: 'moled',
+		color: 'Negro'
+	},
+    {
+		marca: 'Apple',
+		modelo: 'Iphone 12',
+		years: 2018,
+		precio: 130000,
+		ram: 3,
+		pantalla: 'amoled',
+		color: 'Negro'
+	},
+    {
+		marca: 'Apple',
+		modelo: 'Iphone 9',
+		years: 2017,
+		precio: 190000,
+		ram: 8,
+		pantalla: 'moled',
+		color: 'Negro'
+	},
+    {
+		marca: 'Motorola',
+		modelo: 'Razor',
+		years: 2019,
+		precio: 150000,
+		ram: 4,
+		pantalla: 'lcd',
+		color: 'Negro'
+	},
+    {
+		marca: 'Motorola',
+		modelo: 'L44',
+		years: 2017,
+		precio: 89000,
+		ram: 4,
+		pantalla: 'ips',
+		color: 'Blanco'
+	},
+    {
+		marca: 'Motorola',
+		modelo: 'S55',
+		years: 2018,
+		precio: 66000,
+		ram: 4,
+		pantalla: 'ips',
+		color: 'Rojo'
+	},
+    {
+		marca: 'Motorola',
+		modelo: 'T22',
+		years: 2015,
+		precio: 50000,
+		ram: 4,
+		pantalla: 'lcd',
+		color: 'Negro'
+	},
+    {
+		marca: 'Xiaomi',
+		modelo: 'P55',
+		years: 2019,
+		precio: 40000,
+		ram: 4,
+		pantalla: 'Lcd',
+		color: 'Negro'
+	},
+    {
+		marca: 'Xiaomi',
+		modelo: 'Redmi254',
+		years: 2019,
+		precio: 30000,
+		ram: 32,
+		pantalla: 'ips',
+		color: 'Blanco'
+	},
+    {
+		marca: 'Xiaomi',
+		modelo: 'Lost25',
+		years: 2019,
+		precio: 20000,
+		ram: 32,
+		pantalla: 'moled',
+		color: 'Rojo'
+	},
+    {
+		marca: 'Huawei',
+		modelo: 'GSD00',
+		years: 2019,
+		precio: 70000,
+		ram: 4,
+		pantalla: 'amoled',
+		color: 'Negro'
+	},
+    {
+		marca: 'Huawei',
+		modelo: 'MSE55',
+		years: 2015,
+		precio: 80000,
+		ram: 4,
+		pantalla: 'amoled',
+		color: 'Negro'
+	},
+    {
+		marca: 'Huawei',
+		modelo: 'Por24',
+		years: 2018,
+		precio: 90000,
+		ram: 4,
+		pantalla: 'moled',
+		color: 'Negro'
+	},
+    {
+		marca: 'Huawei',
+		modelo: 'MIT42',
+		years: 2016,
 		precio: 120000,
 		ram: 4,
-		pantalla: 'Amoled',
+		pantalla: 'amoled',
 		color: 'Negro'
 	},
     {
 		marca: 'Samsung',
 		modelo: 'a51',
-		years: 2019,
-		precio: 120000,
+		years: 2017,
+		precio: 110000,
 		ram: 4,
-		pantalla: 'Amoled',
-		color: 'Negro'
-	},
-    {
-		marca: 'Samsung',
-		modelo: 'a51',
-		years: 2019,
-		precio: 120000,
-		ram: 4,
-		pantalla: 'Amoled',
-		color: 'Negro'
-	},
-    {
-		marca: 'Samsung',
-		modelo: 'a51',
-		years: 2019,
-		precio: 120000,
-		ram: 4,
-		pantalla: 'Amoled',
-		color: 'Negro'
-	},
-    {
-		marca: 'Samsung',
-		modelo: 'a51',
-		years: 2019,
-		precio: 120000,
-		ram: 4,
-		pantalla: 'Amoled',
-		color: 'Negro'
-	},
-    {
-		marca: 'Samsung',
-		modelo: 'a51',
-		years: 2019,
-		precio: 120000,
-		ram: 4,
-		pantalla: 'Amoled',
-		color: 'Negro'
-	},
-    {
-		marca: 'Samsung',
-		modelo: 'a51',
-		years: 2019,
-		precio: 120000,
-		ram: 4,
-		pantalla: 'Amoled',
-		color: 'Negro'
-	},
-    {
-		marca: 'Samsung',
-		modelo: 'a51',
-		years: 2019,
-		precio: 120000,
-		ram: 4,
-		pantalla: 'Amoled',
-		color: 'Negro'
-	},
-    {
-		marca: 'Samsung',
-		modelo: 'a51',
-		years: 2019,
-		precio: 120000,
-		ram: 4,
-		pantalla: 'Amoled',
-		color: 'Negro'
-	},
-    {
-		marca: 'Samsung',
-		modelo: 'a51',
-		years: 2019,
-		precio: 120000,
-		ram: 4,
-		pantalla: 'Amoled',
-		color: 'Negro'
-	},
-    {
-		marca: 'Samsung',
-		modelo: 'a51',
-		years: 2019,
-		precio: 120000,
-		ram: 4,
-		pantalla: 'Amoled',
-		color: 'Negro'
-	},
-    {
-		marca: 'Samsung',
-		modelo: 'a51',
-		years: 2019,
-		precio: 120000,
-		ram: 4,
-		pantalla: 'Amoled',
-		color: 'Negro'
-	},
-    {
-		marca: 'Samsung',
-		modelo: 'a51',
-		years: 2019,
-		precio: 120000,
-		ram: 4,
-		pantalla: 'Amoled',
-		color: 'Negro'
-	},
-    {
-		marca: 'Samsung',
-		modelo: 'a51',
-		years: 2019,
-		precio: 120000,
-		ram: 4,
-		pantalla: 'Amoled',
-		color: 'Negro'
-	},
-    {
-		marca: 'Samsung',
-		modelo: 'a51',
-		years: 2019,
-		precio: 120000,
-		ram: 4,
-		pantalla: 'Amoled',
-		color: 'Negro'
-	},
-    {
-		marca: 'Samsung',
-		modelo: 'a51',
-		years: 2019,
-		precio: 120000,
-		ram: 4,
-		pantalla: 'Amoled',
-		color: 'Negro'
-	},
-    {
-		marca: 'Samsung',
-		modelo: 'a51',
-		years: 2019,
-		precio: 120000,
-		ram: 4,
-		pantalla: 'Amoled',
-		color: 'Negro'
-	},
-    {
-		marca: 'Samsung',
-		modelo: 'a51',
-		years: 2019,
-		precio: 120000,
-		ram: 4,
-		pantalla: 'Amoled',
-		color: 'Negro'
-	},
-    {
-		marca: 'Samsung',
-		modelo: 'a51',
-		years: 2019,
-		precio: 120000,
-		ram: 4,
-		pantalla: 'Amoled',
-		color: 'Negro'
-	},
-    {
-		marca: 'Samsung',
-		modelo: 'a51',
-		years: 2019,
-		precio: 120000,
-		ram: 4,
-		pantalla: 'Amoled',
+		pantalla: 'ips',
 		color: 'Negro'
 	},
 ];
